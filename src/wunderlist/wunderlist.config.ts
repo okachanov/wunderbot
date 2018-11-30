3e4882faef90cd147e14abb1068cc952d9f6fb40
oleksandr.kachanov <oleksandr.kachanov@innovecs.com>
2018-11-30 15:05:40 +0200

    Extend wunderlist config template

--- src/wunderlist/wunderlist.config.ts
+++ src/wunderlist/wunderlist.config.ts
@@ -1,5 +1,18 @@
 export default {
   wunderlist: {
+    appId: {
+      default: ``,
+      env: 'WUNDERLIST_APP_ID',
+    },
 
+    appSecret: {
+      default: ``,
+      env: 'WUNDERLIST_APP_SECRET',
+    },
+
+    accessToken: {
+      default: ``,
+      env: 'WUNDERLIST_APP_ACCESS_TOKEN',
+    },
   },
 };
